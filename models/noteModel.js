3587d54a95a3e9205e1b20a83793a228b71ebfaa
WisdomNjoroge <mercyn936@gmail.com>
2024-10-01 23:02:42 +0300

    Creating the note model implementation

---
+++ models/noteModel.js
@@ -0,0 +1,21 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../config/database';
+
+const Note = sequelize.define('Note', {
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    content: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+    },
+});
