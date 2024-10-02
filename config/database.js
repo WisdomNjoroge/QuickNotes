175d93088429a22b05b99d9362e097bb59fee254
WisdomNjoroge <mercyn936@gmail.com>
2024-10-02 07:49:20 +0300

    Getting the new config folder

---
+++ config/database.js
@@ -0,0 +1,15 @@
+import { Sequelize } from 'sequelize';
+
+
+const sequelize = new Sequelize(mysql, root, 4216, {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+sequelize
+    .authenticate()
+    .then(() => console.log('Database connected...'))
+    .catch(err => console.log('Error: ' + err));
+
+
+export default sequelize;
