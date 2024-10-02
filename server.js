2758d0283a130752cc5579e39d0a4e277fd8385e
WisdomNjoroge <mercyn936@gmail.com>
2024-10-02 08:05:21 +0300

    Importing necessary files in my server.js file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import sequelize from './config/database.js';
 import authRoutes from './routes/authRoutes.js';
+import noteRoutes from './routes/noteRoutes.js';
 
 
 const app = express();
